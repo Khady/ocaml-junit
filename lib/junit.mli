afe1a6b0e29254b51c56ebb5d253d0227134853a
Louis Roché <louis@louisroche.net>
2016-05-18 19:45:13 +0200

    First version of the interface

---
+++ lib/junit.mli
@@ -0,0 +1,197 @@
+(** JUnit types with variants *)
+
+(** This module defines basic data types for data, attributes and
+    element occuring in JUnit reports.
+
+    It is based on the XSD provided in
+    {{:https://github.com/windyroad/JUnit-Schema} JUnit-schema} git
+    repository and the produced XML is supposed to be accepted by
+    Jenkins.
+*)
+
+(** {1 Categories of elements and attributes} *)
+
+(** This part defines the categories of elements and attributes. *)
+
+(** {2 Attributes} *)
+
+type token
+(** https://www.w3.org/TR/xmlschema-2/#token
+
+    [Definition:] token represents tokenized strings. The ·value space· of
+    token is the set of strings that do not contain the carriage return
+    (#xD), line feed (#xA) nor tab (#x9) characters, that have no
+    leading or trailing spaces (#x20) and that have no internal
+    sequences of two or more spaces. The ·lexical space· of token is
+    the set of strings that do not contain the carriage return (#xD),
+    line feed (#xA) nor tab (#x9) characters, that have no leading or
+    trailing spaces (#x20) and that have no internal sequences of two
+    or more spaces. The ·base type· of token is normalizedString.
+*)
+
+val token : string -> token
+
+type timestamp = string
+(** ISO8601_DATETIME_PATTERN
+
+    TODO: turn it into a date.
+*)
+
+(** {2 Elements} *)
+
+(** {3 Properties}  *)
+
+type property
+
+type properties = property list
+(** Properties (e.g., environment settings) set during test execution. *)
+
+val property :
+  name:token ->
+  value:string ->
+  property
+
+(** {3 Testcases} *)
+
+type error
+(** Indicates that the test errored. An errored test is one that had
+    an unanticipated problem. e.g., an unchecked throwable; or a problem
+    with the implementation of the test. Contains as a text node
+    relevant data for the error, e.g., a stack trace.
+*)
+
+val error :
+  ?message:string ->
+  typ:string ->
+  string ->     (* pcdata? *)
+  error
+(** Creates an error element.
+
+    @param message The error message. e.g., if a java exception is
+    thrown, the return value of getMessage().
+
+    @param typ The type of error that occured. e.g., if a java
+    execption is thrown the full class name of the exception.
+
+    @param description Description of the error.
+*)
+
+type failure
+(** Indicates that the test failed. A failure is a test which the code
+    has explicitly failed by using the mechanisms for that purpose. e.g.,
+    via an assertEquals. Contains as a text node relevant data for the
+    failure, e.g., a stack trace. *)
+
+val failure :
+  ?message:string ->
+  typ:string ->
+  string ->
+  failure
+(** Creates a failure element.
+
+    @param message The message specified in the assert.
+    @param typ The type of the assert.
+    @param description Description of the failure.
+*)
+
+type result =
+  [ `Error of error
+  | `Failure of failure
+  | `Pass
+  | `Skipped    (** Not part of the spec, but available in jenkins. *)
+  ]
+
+type testcase
+
+type testcases = testcase list
+
+val testcase :
+  name:token ->
+  classname:token ->
+  time:float ->
+  result ->
+  testcase
+(** Creates a testcase.
+
+    @param name Name of the test method.
+
+    @param classname Full class name for the class the test method is
+    in.
+
+    @param time Time taken (in seconds) to execute the test.
+
+    @param result Result of the test.
+*)
+
+(** {3 Testsuites} *)
+
+type testsuite
+(** Contains the results of executing a testsuite. *)
+
+type testsuites = testsuite list
+(** Contains an aggregation of testsuite results. *)
+
+val testsuite :
+  package:token ->
+  id:int ->
+  name:token ->
+  timestamp:timestamp ->
+  hostname:token ->
+  tests:int ->
+  failures:int ->
+  errors:int ->
+  time:float ->
+  properties ->
+  testcases ->
+  system_out:string ->
+  system_err:string ->
+  testsuite
+(** Creates a testsuite.
+
+    Attributes
+
+    @param package Derived from the testsuite name in the non-aggregated
+    documents.
+
+    @param id Starts at 0 for the first testsuite and is incremented
+    by 1 for each following testsuite.
+
+    @param name Full class name of the test for non-aggregated
+    testsuite documents. Class name without the package for aggregated
+    testsuites documents.
+
+    @param timestamp When the test was executed. Timezone may not be
+    specified.
+
+    @param hostname Host on which the tests were executed. 'localhost'
+    should be used if the hostname cannot be determined.
+
+    @param tests The total number of tests in the suite.The total
+    number of tests in the suite.
+
+    @param failures The total number of tests in the suite that
+    failed. A failure is a test which the code has explicitly failed
+    by using the mechanisms for that purpose. e.g., via an
+    assertEquals.
+
+    @param errors The total number of tests in the suite that
+    errored. An errored test is one that had an unanticipated
+    problem. e.g., an unchecked throwable; or a problem with the
+    implementation of the test.
+
+    @param time Time taken (in seconds) to execute the tests in the
+    suite.
+
+    Elements
+
+    @param properties Properties (e.g., environment settings) set
+    during test execution.
+
+    @param testcases List of test executed.
+
+    @param system_out Data that was written to standard out while the
+    test was executed.
+
+    @param system_err Data that was written to standard error while
+    the test was executed.
+*)
