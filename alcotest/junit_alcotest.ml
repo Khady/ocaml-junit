2bf5c17bee3f4e88c411e3cef10d7a7931928fd6
Louis <louis@louisroche.net>
2024-12-25 09:31:33 +0100

    alcotest: reraise Skip

--- alcotest/junit_alcotest.ml
+++ alcotest/junit_alcotest.ml
@@ -30,8 +30,9 @@ let wrap_test ?classname handle_result (name, s, test) =
         exn_msg
       |> handle_result;
       raise exn
-    | Alcotest_engine__Core.Skip ->
-      Junit.Testcase.skipped ~name ~classname ~time:0. |> handle_result
+    | Alcotest_engine__Core.Skip as exn ->
+      Junit.Testcase.skipped ~name ~classname ~time:0. |> handle_result;
+      raise exn
     | exn ->
       let exn_msg = Printexc.to_string exn in
       Junit.Testcase.error
